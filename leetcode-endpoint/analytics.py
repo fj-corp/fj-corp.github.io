cde4a947b212924d487b499b567e92083ee9e21f
fj-corp <fahad.jamil.94@gmail.com>
2025-06-23 18:25:00 -0400

    Fixed a major issue with calculating running averages.

--- leetcode-endpoint/analytics.py
+++ leetcode-endpoint/analytics.py
@@ -1,11 +1,17 @@
 class Analytics:
     def __init__(self):
         self.count = 0
-        self.avg_time = 0
+        self.total_time = 0
         self.min_time = float("inf")
         self.max_time = 0
         self.min_time_question = ""
         self.max_time_question = ""
+
+    @property
+    def avg_time(self):
+        if self.count == 0:
+            return 0
+        return round(self.total_time / self.count, 1)
 
     def __str__(self):
         return f"Count: {self.count}, Avg Time: {self.avg_time}, Min Time: {self.min_time}, Max Time: {self.max_time}, Min Time Question: {self.min_time_question}, Max Time Question: {self.max_time_question}"
@@ -14,7 +20,7 @@ class Analytics:
         self.count += 1
 
     def update_times(self, time, question):
-        self.avg_time = (self.avg_time + time) / self.count
+        self.total_time += time
 
         if time < self.min_time:
             self.min_time = time
